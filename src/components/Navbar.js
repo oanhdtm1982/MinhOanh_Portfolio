2471a42b08c61aab5db85de96f3e668a2b3d53ed
MinhhOanhh <19521982@gm.uit.edu.vn>
2022-07-18 13:33:21 +0700

    edit github repo link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -94,7 +94,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/DoVuMinhOanh1982/MinhOanh_Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
